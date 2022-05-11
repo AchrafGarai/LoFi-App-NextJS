3005f496aef519dae9539d9a222811f00cb397d5
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2022-05-11 14:30:25 +0100

    Update Volume.tsx

--- components/Icons/Volume.tsx
+++ components/Icons/Volume.tsx
@@ -13,15 +13,15 @@ function VolumeIcon() {
         d="M11 5L6 9H2V15H6L11 19V5Z"
         stroke="currentColor"
         strokeWidth="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M15.54 8.45996C16.4774 9.3976 17.004 10.6691 17.004 11.995C17.004 13.3208 16.4774 14.5923 15.54 15.53"
         stroke="currentColor"
         strokeWidth="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
