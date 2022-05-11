55eaf44434d0bcf3ae2a593647d3fff66154ee58
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2022-05-11 14:23:16 +0100

    Update index.tsx

--- components/Playlist/index.tsx
+++ components/Playlist/index.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react'
 import player, { usePlayerState } from '@lib/player'
-import { tracks } from './data/data'
-
 const Playlist = () => {
   const state = usePlayerState()
   useEffect(() => {}, [])
